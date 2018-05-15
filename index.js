f06b868774dd1e30d8b3920a11f136a6c08d715c
MrObe13 <oberti.marco@hotmail.it>
2018-05-16 01:56:59 +0200

    First use amaz api

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-const Telegraf = require('telegraf')
-const Extra = require('telegraf/extra')
-const Markup = require('telegraf/markup')
+const Telegraf = require('telegraf');
+const Extra = require('telegraf/extra');
+const Markup = require('telegraf/markup');
+const fetch = require('node-fetch');
+const code = require('./data.js');
 
-const keyboard = Markup.inlineKeyboard([
-    Markup.urlButton('❤️', 'http://telegraf.js.org'),
-    Markup.callbackButton('Delete', 'delete')
-])
+const bot = new Telegraf('581850076:AAHTA-IhMkOiks-KHtcaTm1Frz85vNh48_w');
+bot.help((ctx) => ctx.reply('Help message'));
+let response;
+(async function response() {
+    response = await fetch('http://webservices.amazon.it/onca/xml?Service=AWSECommerceService&AWSAccessKeyId=' + code + '&AssociateTag=grchan-21&Operation=ItemLookup&ItemId=B005U94MVY&Timestamp=2014-08-18T12:00:00Z', {
+        method: 'GET',
+        headers: {"Content-Type": "text/xml"},
+    }).then((res) => {
+        console.log(res)
+        return res
+    })
+})();
+console.log(code)
+bot.on('message', (ctx) => ctx.telegram.sendMessage(ctx.from.id, ("message: " + response)));
 
-const bot = new Telegraf('581850076:AAHTA-IhMkOiks-KHtcaTm1Frz85vNh48_w')
-bot.start((ctx) => ctx.reply('Hello'))
-bot.help((ctx) => ctx.reply('Help message'))
-bot.on('message', (ctx) => ctx.telegram.sendCopy(ctx.from.id, ctx.message, Extra.markup(keyboard)))
-bot.action('delete', ({deleteMessage}) => deleteMessage())
-bot.startPolling()
+bot.startPolling();
